3f284cc3681d7cfced0baff17fde7ed1bee56d84
Cyril Marpaud <cyril.marpaud@bashroom.com>
2023-11-20 17:27:37 +0100

    test: implement fibonacci tests

---
+++ src/fibonacci_closed_form.rs
@@ -0,0 +1,50 @@
+// pub fn fibonacci() {}
+
+#[test]
+fn zero() {
+	assert_eq!(fibonacci(0), 0);
+}
+
+#[test]
+fn one() {
+	assert_eq!(fibonacci(1), 1);
+}
+
+#[test]
+fn three() {
+	assert_eq!(fibonacci(3), 2);
+}
+
+#[test]
+fn five() {
+	assert_eq!(fibonacci(5), 5);
+}
+
+#[test]
+fn ten() {
+	assert_eq!(fibonacci(10), 55);
+}
+
+#[test]
+fn twenty() {
+	assert_eq!(fibonacci(20), 6765);
+}
+
+#[test]
+fn thirty() {
+	assert_eq!(fibonacci(30), 832040);
+}
+
+#[test]
+fn forty() {
+	assert_eq!(fibonacci(40), 102334155);
+}
+
+#[test]
+fn fifty() {
+	assert_eq!(fibonacci(50), 12586269025);
+}
+#[test]
+fn seventy() {
+	assert_eq!(fibonacci(70), 190392490709135);
+}
---
+++ src/lib.rs
@@ -0,0 +1,2 @@
+// pub mod fibonacci_closed_form;
+// pub mod fibonacci_recursive;

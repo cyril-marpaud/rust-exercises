b3681dc5498fc6c4dcaef18a9c7abcfd4b15cbab
Cyril Marpaud <cyril.marpaud@bashroom.com>
2023-11-21 16:40:51 +0100

    feat: implement modularisation solution

---
+++ src/solutions/modularisation.rs
@@ -0,0 +1,35 @@
+mod prelude;
+mod temp_unit;
+
+use temp_unit::TempUnit::{self, *};
+
+pub struct Temp {
+	temp: f64,
+	unit: TempUnit,
+}
+
+impl Temp {
+	fn compute(&self, to: &TempUnit) -> f64 {
+		match (&self.unit, to) {
+			(Celsius, Farenheit) => 9.0 * self.temp / 5.0 + 32.0,
+			(Celsius, Kelvin) => self.temp + 273.15,
+			(Farenheit, Celsius) => 5.0 * (self.temp - 32.0) / 9.0,
+			(Farenheit, Kelvin) => 5.0 * (self.temp - 32.0) / 9.0 + 273.15,
+			(Kelvin, Celsius) => self.temp - 273.15,
+			(Kelvin, Farenheit) => 9.0 * (self.temp - 273.15) / 5.0 + 32.0,
+			_ => self.temp,
+		}
+	}
+
+	/// Convert a `Temp` into another unit.
+	pub fn convert(&mut self, to: TempUnit) -> &mut Self {
+		self.temp = self.compute(&to);
+		self.unit = to;
+		self
+	}
+
+	/// Create a new `Temp` object.
+	pub fn new(temp: f64, unit: TempUnit) -> Self {
+		Self { temp, unit }
+	}
+}
---
+++ src/solutions/modularisation/prelude.rs
@@ -0,0 +1,1 @@
+pub use super::{temp_unit::TempUnit::*, Temp};

60297f2e7865264399ba9eeb25bf52b4eea8ad85
Cyril Marpaud <cyril.marpaud@bashroom.com>
2024-06-19 15:35:49 +0200

    fix: Display implementation

--- src/deserialize/temp_unit.rs
+++ src/deserialize/temp_unit.rs
@@ -12,11 +12,11 @@ impl Display for TempUnit {
 	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
 		write!(
 			f,
-			"°{}",
+			"{}",
 			match self {
-				TempUnit::Celsius => 'C',
-				TempUnit::Fahrenheit => 'F',
-				TempUnit::Kelvin => 'K',
+				TempUnit::Celsius => "°C",
+				TempUnit::Fahrenheit => "°F",
+				TempUnit::Kelvin => "K",
 			}
 		)
 	}

6437151e91c58e33b1e2c5f9552b5ed4d14de996
Cyril Marpaud <cyril.marpaud@bashroom.com>
2024-10-07 17:51:17 +0200

    docs: document e05 properly

--- e05_quiz/src/lib.rs
+++ e05_quiz/src/lib.rs
@@ -1,4 +1,10 @@
-//! - Uncomment the `quiz` module
+//! # e05_1_quiz - Error Handling
+//!
+//! This exercise focuses on error handling. We will refactor the quiz library so that it's
+//! functions return a proper `Result` instead of panicking.
+//!
+//! We will use the `thiserror` crate to create a single error type for all errors:
+//!
 //! - Add `thiserror` to the dependencies
 //! - Create a single error type for all errors
 //! - Make all functions return a `Result`

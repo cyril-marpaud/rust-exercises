412db558d9bc6360a3b01c2f2f861b6f35fd6331
Cyril Marpaud <cyril.marpaud@bashroom.com>
2024-10-07 17:10:40 +0200

    docs: document e03 properly

--- e03_temperature/src/lib.rs
+++ e03_temperature/src/lib.rs
@@ -1,11 +1,25 @@
-//! - Uncomment the `temperature` module
-//! - Implement the necessary components
-//!   - A way to store a temperature (`Temp`)
-//!   - Support for Celsius, Fahrenheit & Kelvin units (`TempUnit`)
-//!   - Instantiation and convertion methods (`new()` & `convert()`)
-//! - Run the tests
-//!   `cargo test -- temperature`
+//! # e03_1_temperature - Temperature Conversion
 //!
-//! conversion table: https://byjus.com/temperature-conversion-formula/
+//! Implement a temperature conversion library that can convert between Celsius, Fahrenheit and
+//! Kelvin.
+//!
+//! It should have a `Temp` struct that stores the temperature and a `TempUnit` enum that represents
+//! the temperature unit with support for Celsius, Fahrenheit and Kelvin.
+//!
+//! The following methods shall be available:
+//!
+//! - `new` - Instantiates a new `Temp` struct
+//! - `convert` - Converts the temperature to the specified unit
+//!
+//! ## Conversion formulas:
+//!
+//! | From | To | Formula |
+//! |:----:|:--:|:--------|
+//! | C | F | F = 9 ⨉ C ÷ 5 + 32 |
+//! | C | K | K = C + 273.15 |
+//! | F | C | C = 5 ⨉ (F - 32) ÷ 9 |
+//! | F | K | K = 5 ⨉ (F - 32) ÷ 9 + 273.15 |
+//! | K | C | C = K - 273.15 |
+//! | K | F | F = 9 ⨉ (K - 273.15) ÷ 5 + 32 |
 
 // mod e03_1_temperature;

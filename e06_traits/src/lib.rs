485ee029a3755b56de2ad5e4617103355f6a036a
Cyril Marpaud <cyril.marpaud@bashroom.com>
2024-10-07 17:51:21 +0200

    docs: document e06 properly

--- e06_traits/src/lib.rs
+++ e06_traits/src/lib.rs
@@ -1,8 +1,15 @@
-//! - Uncomment the `traits` module
-//! - Implement the following traits
-//!   - [`Debug`](https://doc.rust-lang.org/std/fmt/trait.Debug.html), [`Display`](https://doc.rust-lang.org/std/fmt/trait.Display.html) , and [`Default`](https://doc.rust-lang.org/std/default/trait.Default.html)for `Temp` and `TempUnit`
-//!   - [`PartialEq`](https://doc.rust-lang.org/std/cmp/trait.PartialEq.html) and [`PartialOrd`](https://doc.rust-lang.org/std/cmp/trait.PartialOrd.html) for `Temp`
-//! - Run the tests
-//!   `cargo test -- traits`
+//! # e06_1_traits - Traits
+//!
+//! In this exercise, we will implement common traits for our Temp and TempUnit types to ensure
+//! idiomatic code.
+//!
+//! Uncomment each test one by one and make them pass by implementing the following traits:
+//! - on both `Temp` and `TempUnit`:
+//!   - [`Debug`](https://doc.rust-lang.org/std/fmt/trait.Debug.html)
+//!   - [`Display`](https://doc.rust-lang.org/std/fmt/trait.Display.html)
+//!   - [`Default`](https://doc.rust-lang.org/std/default/trait.Default.html)
+//! - on `Temp` only:
+//!   - [`PartialEq`](https://doc.rust-lang.org/std/cmp/trait.PartialEq.html)
+//!   - [`PartialOrd`](https://doc.rust-lang.org/std/cmp/trait.PartialOrd.html)
 
 // mod e06_1_traits;

9513a6451145e8c94f7621001891404cf9f10c3a
Cyril Marpaud <cyril.marpaud@bashroom.com>
2024-10-07 17:10:51 +0200

    docs: document e04 properly

--- e04_modularization/src/lib.rs
+++ e04_modularization/src/lib.rs
@@ -1,10 +1,11 @@
-//! - Uncomment the `modularization` module
-//! - Implement the necessary components
-//!   - Move `TempUnit` into a specific module
-//!   - Create a `prelude`
-//!     - It should export `Temp`, `Celsius`, `Fahrenheit`, and `Kelvin`
-//!   - Use rust-analyzer as much as you can ! (💡 or **`ctrl`**+**`.`**)
-//! - Pass the tests
-//!   `cargo test -- modularization`
+//! # e04_1_modularization - Modularization
+//!
+//! In this exercise, we will modularize the temperature conversion library. The goal is to have a
+//! more organized codebase with a clear separation of concerns.
+//!
+//! Start by moving the `TempUnit` enum into it's own module. Then, create a `prelude` module that
+//! exports `Temp`, `Celsius`, `Fahrenheit`, and `Kelvin` (the enum's variants, not the enum itself).
+//!
+//! Use rust-analyzer as much as you can ! (💡 or **`ctrl`**+**`.`**)
 
 // mod e04_1_modularization;

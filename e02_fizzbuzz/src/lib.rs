f99908e971067d4aacd024cdc069ad0c40e97f7e
Cyril Marpaud <cyril.marpaud@bashroom.com>
2024-10-07 16:33:18 +0200

    docs: document e02 properly

--- e02_fizzbuzz/src/lib.rs
+++ e02_fizzbuzz/src/lib.rs
@@ -1,2 +1,13 @@
+//! # e02_1_imperative - FizzBuzz, imperative paradigm
+//!
+//! Write a function that takes an integer `n` and returns a vector of strings. The vector should
+//! contain the numbers from 1 to `n`, but replacing multiples of 3 with "Fizz", multiples of 5 with
+//! "Buzz", and multiples of both 3 and 5 with "FizzBuzz".
+//!
+//! # e02_2_functionnal - FizzBuzz, functional paradigm
+//!
+//! This time, use the functional paradigm to solve the problem. Write an iterator that generates
+//! the FizzBuzz sequence from 1 to `n`. The iterator should yield `String`s.
+
 // mod e02_1_imperative;
 // mod e02_2_functionnal;

ea0805f5ced15314229c4a1399bd8897817582cf
Cyril Marpaud <cyril.marpaud@bashroom.com>
2024-10-07 16:32:55 +0200

    docs: document e01 properly

--- e01_fibonacci/src/lib.rs
+++ e01_fibonacci/src/lib.rs
@@ -1,32 +1,22 @@
-//! - Clone or download the exercise repo
-//!   - [`git clone https://gitlab.com/cmp-trainings/rust/rust-exercises.git`](https://gitlab.com/cmp-trainings/rust/rust-exercises)
-//! - In `lib.rs`, uncomment `fibonacci_recursive`
-//! - In `fibonacci_recursive.rs`, implement the function
-//!   - `fib(0) = 0`, `fib(1) = 1`, `fib(n) = fib(n-1) + fib(n-2)`
-//! - The exercise is OK when the tests pass
-//!   - `cargo test -- fibonacci_recursive`
+//! # e01_1_recursive - Recursive Fibonacci sequence
 //!
-//! # TP#2: Fibonacci... Faster
-//! - Uncomment the module `fibonacci_closed_form`
-//! - Implement the function
-//! - Run the tests
+//! The Fibonacci sequence is defined as follows:
+//! - `fib(0) = 0`
+//! - `fib(1) = 1`
+//! - `fib(n) = fib(n-1) + fib(n-2)`
 //!
-//! - $fib(n) = \frac{\phi^n - \psi^n}{\sqrt{5}}$
-//!   - $\phi = \frac{1 + \sqrt{5}}{2}$
-//!   - $\psi = \frac{1 - \sqrt{5}}{2} = 1 - \phi$
+//! Write a recursive function that returns the n-th number in the Fibonacci sequence. You may use
+//! `u64` as the parameter and return types.
 //!
-//! - tools:
-//!   - `4f64.sqrt() = 2.0`
-//!   - `2f64.powi(2) = 4.0`
-//!   - `4.5f64.round() = 5.0`
-//!   - `2 as f64 = 2.0`
-
-//! <!--
-//! Show configuration for:
-//! - automatic linting
-//! - automatic formatting
-//! - documenting
-//! -->
+//! # e01_2_closed_form - Closed-form Fibonacci sequence
+//!
+//! The function we previously wrote quickly becomes very slow as `n` grows larger. We can avoid
+//! this by using a closed-form expression for the Fibonacci sequence.
+//!
+//! The closed-form expression for the Fibonacci sequence can be found at
+//! <https://en.wikipedia.org/wiki/Fibonacci_sequence#Closed-form_expression>.
+//!
+//! Write a function that returns the n-th number in the Fibonacci sequence using the closed-form.
 
 // mod e01_1_recursive;
 // mod e01_2_closed_form;

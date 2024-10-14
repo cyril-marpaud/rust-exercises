96288530032b0051d5f81f22e4b523ca830fde08
Cyril Marpaud <cyril.marpaud@bashroom.com>
2024-10-14 16:13:06 +0200

    docs(e06): improve instructions

--- e06_traits/src/lib.rs
+++ e06_traits/src/lib.rs
@@ -1,15 +1,51 @@
 //! # e06_1_traits - Traits
 //!
-//! In this exercise, we will implement common traits for our Temp and TempUnit types to ensure
-//! idiomatic code.
+//! ## Overview
+//! Enhance the idiomatic use of Rust by implementing common traits for the `Temp` and `TempUnit` types.
+//! Implementing these traits will ensure that our temperature conversion library adheres to Rust's design principles, enhancing utility and compatibility.
 //!
-//! Uncomment each test one by one and make them pass by implementing the following traits:
-//! - on both `Temp` and `TempUnit`:
-//!   - [`Debug`](https://doc.rust-lang.org/std/fmt/trait.Debug.html)
-//!   - [`Display`](https://doc.rust-lang.org/std/fmt/trait.Display.html)
-//!   - [`Default`](https://doc.rust-lang.org/std/default/trait.Default.html)
-//! - on `Temp` only:
-//!   - [`PartialEq`](https://doc.rust-lang.org/std/cmp/trait.PartialEq.html)
-//!   - [`PartialOrd`](https://doc.rust-lang.org/std/cmp/trait.PartialOrd.html)
+//! ## Task
+//! Implement several traits to improve the functionality and usability of the `Temp` and `TempUnit` types:
+//! - For both `Temp` and `TempUnit`:
+//!   - [`Debug`](https://doc.rust-lang.org/std/fmt/trait.Debug.html): Enables logging and debugging by formatting the output using the `{:?}` formatter. `Debug` is [derivable](https://doc.rust-lang.org/std/fmt/trait.Debug.html#examples).
+//!
+//!     <details>
+//!     <summary>Example of deriving <code>Debug</code></summary>
+//!
+//!     Simply add `#[derive(Debug)]` above your struct or enum definition:
+//!     ```ignore
+//!     #[derive(Debug)]
+//!     pub struct Temp {
+//!         temp: f64,
+//!         unit: TempUnit,
+//!     }
+//!     ```
+//!
+//!     </details>
+//!
+//!   - [`Display`](https://doc.rust-lang.org/std/fmt/trait.Display.html): Allows custom string representation, making objects printable in a user-friendly format. Not derivable, must be manually implemented.
+//!
+//!     <details>
+//!     <summary>Example of a manual implementation</summary>
+//!
+//!     ```ignore
+//!     impl Display for TempUnit { ... }
+//!     ```
+//!
+//!     </details>
+//!
+//!   - [`Default`](https://doc.rust-lang.org/std/default/trait.Default.html): Provides a mechanism to create default values of these types. `Default` is [derivable](https://doc.rust-lang.org/std/default/trait.Default.html#derivable).
+//! - For `Temp` only:
+//!   - [`PartialEq`](https://doc.rust-lang.org/std/cmp/trait.PartialEq.html): Enables comparison for equality. `PartialEq` is [derivable](https://doc.rust-lang.org/std/cmp/trait.PartialEq.html#derivable).
+//!   - [`PartialOrd`](https://doc.rust-lang.org/std/cmp/trait.PartialOrd.html): Allows objects to be compared, which is essential for ordering. `PartialOrd` is [derivable](https://doc.rust-lang.org/std/cmp/trait.PartialOrd.html#derivable).
+//!
+//! ## Instructions
+//! - **Step 1:** Uncomment each unit test one by one.
+//! - **Step 2:** Implement the required traits to make the tests pass. Use Rust Analyzer's quick fix "Implement Missing Members" to automatically generate method prototypes.
+//!
+//! ## Tips
+//! - Utilize Rust's documentation to understand traits' use and applicability.
+//! - Use Rust Analyzer to speed up trait implementation:
+//!   - Click the yellow light bulb (💡) or use **`ctrl`**+**`.`** when a trait is not fully implemented to bring up the quick fixes menu.
 
-// mod e06_1_traits;
+// pub mod e06_1_traits;

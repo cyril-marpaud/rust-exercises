4a0455f148f5c457f9bd1c2be91a22b8af7364b7
Cyril Marpaud <cyril.marpaud@bashroom.com>
2024-10-14 15:17:01 +0200

    docs(e03): improve instructions

--- e03_temperature/src/lib.rs
+++ e03_temperature/src/lib.rs
@@ -1,25 +1,30 @@
 //! # e03_1_temperature - Temperature Conversion
 //!
-//! Implement a temperature conversion library that can convert between Celsius, Fahrenheit and
-//! Kelvin.
+//! ## Overview
+//! Implement a temperature conversion library that handles conversions between Celsius (C), Fahrenheit (F), and Kelvin (K).
 //!
-//! It should have a `Temp` struct that stores the temperature and a `TempUnit` enum that represents
-//! the temperature unit with support for Celsius, Fahrenheit and Kelvin.
+//! ## Components
+//! - `Temp` struct: Holds a temperature value along with its unit.
+//! - `TempUnit` enum: Defines the units of temperature, such as Celsius, Fahrenheit, and Kelvin.
 //!
-//! The following methods shall be available:
+//! ## Methods
+//! - `new`: Creates a new `Temp` instance with specified temperature and unit.
+//!   ```ignore
+//!   let temp = Temp::new(25.0, TempUnit::Celsius);
+//!   ```
+//! - `convert`: Modifies the `Temp` instance to convert its temperature to a specified unit.
+//!   ```ignore
+//!   temp.convert(TempUnit::Fahrenheit);
+//!   ```
 //!
-//! - `new` - Instantiates a new `Temp` struct
-//! - `convert` - Converts the temperature to the specified unit
-//!
-//! ## Conversion formulas:
-//!
-//! | From | To | Formula |
-//! |:----:|:--:|:--------|
-//! | C | F | F = 9 ⨉ C ÷ 5 + 32 |
-//! | C | K | K = C + 273.15 |
-//! | F | C | C = 5 ⨉ (F - 32) ÷ 9 |
-//! | F | K | K = 5 ⨉ (F - 32) ÷ 9 + 273.15 |
-//! | K | C | C = K - 273.15 |
-//! | K | F | F = 9 ⨉ (K - 273.15) ÷ 5 + 32 |
+//! ## Conversion Formulas
+//! | From | To   | Formula                           |
+//! |:----:|:----:|:---------------------------------:|
+//! | C    | F    | F = 9 ⨉ C ÷ 5 + 32                |
+//! | C    | K    | K = C + 273.15                    |
+//! | F    | C    | C = 5 ⨉ (F - 32) ÷ 9              |
+//! | F    | K    | K = 5 ⨉ (F - 32) ÷ 9 + 273.15     |
+//! | K    | C    | C = K - 273.15                    |
+//! | K    | F    | F = 9 ⨉ (K - 273.15) ÷ 5 + 32     |
 
 // mod e03_1_temperature;

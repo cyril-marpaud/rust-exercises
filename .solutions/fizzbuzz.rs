11fadb0c3a30a7893218eaabd707276671833ad5
Cyril Marpaud <-e>
2025-06-09 16:28:24 +0200

    fix(e02): fix solutions' case

--- .solutions/fizzbuzz.rs
+++ .solutions/fizzbuzz.rs
@@ -1,8 +1,8 @@
 pub fn fizzbuzz(n: u32) -> String {
 	match (n % 3, n % 5) {
-		(0, 0) => String::from("fizzbuzz"),
-		(0, _) => String::from("fizz"),
-		(_, 0) => String::from("buzz"),
+		(0, 0) => String::from("FizzBuzz"),
+		(0, _) => String::from("Fizz"),
+		(_, 0) => String::from("Buzz"),
 		_ => n.to_string(),
 	}
 }
--- .solutions/fizzbuzz_no_allocation.rs
+++ .solutions/fizzbuzz_no_allocation.rs
@@ -2,9 +2,9 @@ use std::borrow::Cow;
 
 pub fn fizzbuzz(n: u32) -> Cow<'static, str> {
 	match (n % 3, n % 5) {
-		(0, 0) => Cow::Borrowed("fizzbuzz"),
-		(0, _) => Cow::Borrowed("fizz"),
-		(_, 0) => Cow::Borrowed("buzz"),
+		(0, 0) => Cow::Borrowed("FizzBuzz"),
+		(0, _) => Cow::Borrowed("Fizz"),
+		(_, 0) => Cow::Borrowed("Buzz"),
 		_ => Cow::Owned(n.to_string()),
 	}
 }
